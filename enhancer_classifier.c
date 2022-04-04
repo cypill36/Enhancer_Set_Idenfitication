6f99c3fdcc1de9d3c0304a7d99a45682f6e8f07f
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-04-04 11:32:50 -0500

    add example configuration files

--- enhancer_classifier.c
+++ enhancer_classifier.c
@@ -11,22 +11,48 @@
 //                  results. 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main() {
+#define ARGNUM      2
+#define PARAM_POS   1
+
+int main(int argc, char *argv[]) {
 
     // Check for valid number of arguments passed in
+    if (argc != ARGNUM) {
+        fprintf(stderr, "Usage: %s <param_file>\n", argv[0]);
+        exit(1);
+    }
 
-
+    ///////////////////////////////////////////////////////////////////////////
     // Check for valid argument inputs
+    // Check for valid PARAM_FILE
+    const char *filename = argv[PARAM_POS];
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("file cannot be opened");
+        exit(2);
+    }
+
+    /* Check for valid DATA path */
+
+    /* Check for valid OUTPUT path */
+
+    /* Check for valid ML_PLUGIN */
+
+    /* Check for valid ANALYSIS_PLUGIN */
+
+    /* Preprocess input files for machine learning tool */
 
 
-    // Preprocess input files for machine learning tool
+    /* Invoke machine learning plugin tool
+     * and pass in the parameters and input file
+     */
 
-
-    // Invoke machine learning LS-GKM tool and pass in the parameters and input file
-    // ./gkmtrain -m 96000 -T 16 $DATA_PATH/shared_5files_1k.fa $DATA_PATH/uniq_5files_1k.fa test_gkmtrain
-
-    // Invoke python ROC curve generation script 
+    /* Invoke analysis plugin tool and pass in ML_PLUGIN output files */
 
     printf("Program Done!\n");
     return 0;
