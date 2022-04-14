7254052b96616a37f5b2c9776d9758cc30948fe5
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-04-14 16:50:05 -0500

    add params examples to config file

--- enhancer_classifier.c
+++ enhancer_classifier.c
@@ -14,7 +14,10 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
 
 #define ARGNUM      2
 #define PARAM_POS   1
@@ -37,22 +40,43 @@ int main(int argc, char *argv[]) {
         exit(2);
     }
 
-    /* Check for valid DATA path */
+    // Check for valid WORKING_DIR path 
+    DIR* dir = opendir("working");
+    if (dir) {
+        closedir(dir);
+    } else if (ENOENT == errno) {
+        fprintf(stderr, "The working directory does not exist.\n");
+        exit(1);
+    } else {
+        perror("Error in WORKING_DIR parameter.\n");
+        exit(2);
+    }      
 
-    /* Check for valid OUTPUT path */
+    // Check for valid ML_PLUGIN 
 
-    /* Check for valid ML_PLUGIN */
+    // Check for valid ANALYSIS_PLUGIN 
 
-    /* Check for valid ANALYSIS_PLUGIN */
+    // Create Output Dir for ML and Plotting if specified does not exist    
+    int ret = mkdir("ML_output", );
+    if (ret == -1 && EEXIST != errno) {
+        perror("Error in Creating ML Output Directory\n");
+        exit(3);
+    }                                                                           
+    
+    ret = mkdir("Plotting_output", );
+    if (ret == -1 && EEXIST != errno) {
+        perror("Error in Creating Plotting Output Directory\n");
+        exit(3);
+    } 
 
-    /* Preprocess input files for machine learning tool */
+
+    // Preprocess input files and parameters for machine learning tool 
 
 
-    /* Invoke machine learning plugin tool
-     * and pass in the parameters and input file
-     */
+    // Invoke ML plugin tool and pass in the parameters and input file 
 
-    /* Invoke analysis plugin tool and pass in ML_PLUGIN output files */
+
+    // Invoke analysis plugin tool and pass in ML_PLUGIN output files 
 
     printf("Program Done!\n");
     return 0;
